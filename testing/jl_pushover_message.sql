f1d3838484ece5f0fe6defee7969bd42ce37fda9
Jules <Julian.lane@oracle.com>
2016-06-10 11:46:09 +0100

    SQL Dev Proc text changed

--- testing/jl_pushover_message.sql
+++ testing/jl_pushover_message.sql
@@ -17,7 +17,7 @@ set define off;
         l_user  varchar2(100);
        begin
         --
-        -- these not used currently xxx
+        -- these not used currently v2
         --
         l_token := 'abMWr23QPZiXTWPvxDbGCzQcdSFp4Q';
         l_user := 'u9Tm4UyBgai3PCwyrPwgFWuz12TXYU';
