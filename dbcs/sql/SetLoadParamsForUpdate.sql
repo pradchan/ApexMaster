ef3a1461a1a481ee2a6516be1224d428c72d9e8d
Jules <Julian.lane@oracle.com>
2016-06-15 15:55:17 +0100

    xxx

--- dbcs/sql/SetLoadParamsForUpdate.sql
+++ dbcs/sql/SetLoadParamsForUpdate.sql
@@ -1,8 +1,16 @@
 --
 -- Set import over-ride params to workspace and schema = JULES
 --
+declare
+    l_workspace_id number;
 begin
-  SetLoadParamsForUpdate;
+    select workspace_id into l_workspace_id
+      from apex_workspaces
+      where workspace = 'JULES';
+    --
+    apex_application_install.set_workspace_id( l_workspace_id );
+    apex_application_install.generate_offset;
+    apex_application_install.set_schema( 'JULES' );
 end;
 --
 -- Import the exported app
