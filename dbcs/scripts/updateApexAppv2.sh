6544e64e77b38e45ace98c2887a2117ff0a949b8
Jules <Julian.lane@oracle.com>
2016-06-15 16:05:11 +0100

    xxx

--- dbcs/scripts/updateApexAppv2.sh
+++ dbcs/scripts/updateApexAppv2.sh
@@ -6,13 +6,13 @@ dbcs_sys_password=$3
 
 echo "copying SetParams and Apex export file to target:"
 
-scp -o StrictHostKeyChecking=no -i dbcs/labkey dbcs/sql/SetLoadParamsForUpdate.sql oracle@$dbcs_ip:.
+scp -o StrictHostKeyChecking=no -i dbcs/labkey dbcs/sql/UpdateApp.sql oracle@$dbcs_ip:.
 scp -o StrictHostKeyChecking=no -i dbcs/labkey dbcs/sql/updateApexApp.sql oracle@$dbcs_ip:.
 
 echo "..done."
 
 echo "Setting Import Params and Importing App:"
 
-ssh -o StrictHostKeyChecking=no -i dbcs/labkey oracle@$dbcs_ip "sqlplus $dbcs_sys/$dbcs_sys_password@PDB1 AS SYSDBA @SetLoadParamsForUpdate.sql"
+ssh -o StrictHostKeyChecking=no -i dbcs/labkey oracle@$dbcs_ip "sqlplus $dbcs_sys/$dbcs_sys_password@PDB1 AS SYSDBA @UpdateApp.sql"
 
 echo "..done."
