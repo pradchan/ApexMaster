0277f885c407f533671c625615c9a7e27c019b99
Jules <Julian.lane@oracle.com>
2016-09-23 16:51:05 +0100

    use new ssh key

--- dbcs/scripts/copySQLFilesv2.sh
+++ dbcs/scripts/copySQLFilesv2.sh
@@ -3,11 +3,11 @@
 dbcs_ip=$1
 
 echo "copying SQL files:"
-scp -o StrictHostKeyChecking=no -i dbcs/JulesPrivateSSHKeySept2016 dbcs/sql/*.sql oracle@$dbcs_ip:.
+scp -o StrictHostKeyChecking=no -i dbcs/labkey dbcs/sql/*.sql oracle@$dbcs_ip:.
 echo "..done."
 
 echo "copying SQL DATA file:"
-scp -o StrictHostKeyChecking=no -i dbcs/JulesPrivateSSHKeySept2016 dbcs/data/loadApexData.sql oracle@$dbcs_ip:.
+scp -o StrictHostKeyChecking=no -i dbcs/labkey  dbcs/data/loadApexData.sql oracle@$dbcs_ip:.
 echo "..done."
 
 
