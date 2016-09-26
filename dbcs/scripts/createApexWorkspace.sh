4de07c8b27865ae671f0f5e2f3838728b48eac5a
Jules <Julian.lane@oracle.com>
2016-09-26 17:40:05 +0100

    Tidy diagnostics

--- dbcs/scripts/createApexWorkspace.sh
+++ dbcs/scripts/createApexWorkspace.sh
@@ -4,8 +4,8 @@ dbcs_ip=$1
 dbcs_sys=$2
 dbcs_sys_password=$3
 
-echo "Creating Apex workspace on target Database..:"
 echo "--//--"
+echo "Creating Apex workspace on target Database..:"
 ssh -o StrictHostKeyChecking=no -i dbcs/labkey oracle@$dbcs_ip "sqlplus $dbcs_sys/$dbcs_sys_password@PDB1 AS SYSDBA @createApexWorkspace.sql"
 echo "..done."
 echo "--//--"
