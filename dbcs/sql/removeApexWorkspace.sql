cdc44911e9bc714378a02304323cac3eba165643
PRADCHAN.IN <pradeep.chandramouli@oracle.com>
2016-10-27 13:17:24 +0530

    Signed-off-by: PRADCHAN.IN <pradeep.chandramouli@oracle.com>

--- dbcs/sql/removeApexWorkspace.sql
+++ dbcs/sql/removeApexWorkspace.sql
@@ -1,16 +1,12 @@
 set define off verify off feedback off
 whenever sqlerror exit sql.sqlcode rollback
 
-prompt  --//--
-prompt  Calling APEX API..
-
 begin
   APEX_INSTANCE_ADMIN.REMOVE_WORKSPACE('JULES','N','N');
 end;
 /
 
 set verify on feedback on define on
-prompt  --//--
 prompt  ...done
 
 exit
